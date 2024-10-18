cab42243f7ed9243b1d572d275c66ec5ea208764
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-18 11:19:02 +0300

    add sidebar and navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+export default function Navbar() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [lastYPosition, setLastYPosition] = useState<number>(0);
+
+  function handleScroll() {
+    const windowScrollY = window.scrollY;
+
+    if (windowScrollY > lastYPosition) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+
+    setLastYPosition(windowScrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lastYPosition]);
+
+  return (
+    <nav
+      className={`bg-gray-100 text-black fixed w-full ${
+        isVisible ? "fixed" : "hidden"
+      }`}
+    >
+      <ul className="flex items-center justify-center h-20">
+        <Link href="/">
+          <li className="text-3xl font-extrabold">3Sided</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,3 @@
+export default function Sidebar() {
+  return <div className="w-96 h-full bg-gray-200"></div>;
+}
