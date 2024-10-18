85fae6aa963a316fb3d3cd2e6fcbf5b1041759be
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-18 11:35:22 +0300

    add placeholder data and charm card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import CharmCard from "@/components/card";
 import Sidebar from "@/components/sidebar";
+import { charms } from "@/lib/placeholder-data";
 
 export default function Home() {
   return (
     <div className="pt-20 bg-gray-300 w-full h-screen flex">
       <Sidebar />
-      <div className="text-center w-full">Home Page</div>
+      <div className="text-center w-full pt-10 pl-10 grid grid-cols-4">
+        {charms.map((charm) => (
+          <div key={charm.id}>
+            <CharmCard
+              label={charm.label}
+              price={charm.price}
+              imageSrc={charm.imageSrc}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/lib/placeholder-data.ts
@@ -0,0 +1,22 @@
+const charms = [
+  {
+    id: 1,
+    imageSrc: "/jewel_1.jpg",
+    label: "This charm is defininately him.",
+    price: 50,
+  },
+  {
+    id: 2,
+    imageSrc: "/jewel_2.jpg",
+    label: "This charm gives 1000 aura.",
+    price: 69,
+  },
+  {
+    id: 1,
+    imageSrc: "/jewel_2.jpg",
+    label: "Wearing this charm makes you a princess",
+    price: 45,
+  },
+];
+
+export { charms };
