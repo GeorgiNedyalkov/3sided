8773c2a43ad8a698b8b814ee3628d4ec282ecb6e
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-23 18:37:36 +0300

    bugfix removing element from cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -16,7 +16,7 @@ export default function CartPage() {
               of <span>{item.price * item.quantity} лв.</span>
               <button
                 className="bg-red-500 p-2 rounded-xl"
-                onClick={() => removeFromCart(item.id)}
+                onClick={() => removeFromCart(item)}
               >
                 Remove from cart
               </button>
