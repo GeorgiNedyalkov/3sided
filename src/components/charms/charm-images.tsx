29b81ab6708730d44b3168b36bed49c379439b7a
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-31 16:13:17 +0200

    add horizontal scrollbar to charm images

--- src/components/charms/charm-images.tsx
+++ src/components/charms/charm-images.tsx
@@ -10,7 +10,7 @@ export default function CharmImages({
   charmImages: string[];
 }) {
   return (
-    <div className="flex w-full gap-4 p-4 overflow-y-auto">
+    <div className="flex w-full gap-4 p-4 overflow-x-scroll">
       {charmImages.map((src, index) => (
         <Image
           key={index}
