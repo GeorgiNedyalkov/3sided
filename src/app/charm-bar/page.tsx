2a1c936758bd44714c8c006d294596ca9ac893c0
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-31 15:13:42 +0200

    choose bracelet

--- src/app/charm-bar/page.tsx
+++ src/app/charm-bar/page.tsx
@@ -13,17 +13,58 @@ const charmImages = [
   "/charm_5.webp",
 ];
 
-// These too
+const necklaces = [
+  {
+    id: "1",
+    src: "/necklace.webp",
+    settings: [
+      { top: "78%", left: "42%", rotation: "0deg" },
+      { top: "62%", right: "16%", rotation: "-45deg" },
+      { top: "62%", left: "18%", rotation: "45deg" },
+      { top: "40%", right: "8%", rotation: "-45deg" },
+      { top: "40%", left: "10%", rotation: "45deg" },
+    ],
+  },
+  {
+    id: "2",
+    src: "/necklace2.png",
+    settings: [
+      { top: "80%", left: "42%", rotation: "0deg" },
+      { top: "65%", right: "23%", rotation: "-45deg" },
+      { top: "65%", left: "23%", rotation: "45deg" },
+      { top: "45%", right: "15%", rotation: "-45deg" },
+      { top: "45%", left: "15%", rotation: "45deg" },
+    ],
+  },
+];
 
-const braceletImages = ["/necklace.webp"];
+type CharmPosition = {
+  top?: string;
+  bottom?: string;
+  left?: string;
+  right?: string;
+  rotation?: string;
+};
+
+type Necklace = {
+  id: string;
+  src: string;
+  settings: CharmPosition[];
+};
 
 export default function CharmBar() {
-  const backgroundImg = "/necklace.webp";
+  const [selectedNecklace, setSelectedNecklace] = useState<Necklace>(
+    necklaces[0]
+  );
   const [selectedCharm, setSelectedCharm] = useState<string | null>(null);
   const [selectedPosition, setSelectedPosition] = useState<number>(0);
   const [charmPositions, setCharmPositions] = useState(
     Array(charmImages.length).fill(null)
   );
+
+  function handleNeckalceSelect(necklace: Necklace) {
+    setSelectedNecklace(necklace);
+  }
 
   function handleCharmSelect(imageSrc: string, position: number) {
     setSelectedCharm(imageSrc);
@@ -42,7 +83,7 @@ export default function CharmBar() {
 
   return (
     <div className="grid mt-4 h-screen w-full grid-cols-2 grid-rows-7">
-      <div className="col-start-2 row-start-1">
+      <div className="col-start-2">
         <h1 className="text-3xl font-semibold mb-4">
           Create your own personalized bracelet
         </h1>
@@ -52,8 +93,24 @@ export default function CharmBar() {
           Click on the charms for each position to see how it looks like.
         </p>
       </div>
+      <div className="col-start-2">
+        <div>Choose a bracelet</div>
+        <div className="flex gap-4">
+          {necklaces.map((necklace, index) => (
+            <Image
+              key={necklace.id}
+              src={necklace.src}
+              onClick={() => handleNeckalceSelect(necklace)}
+              width={100}
+              height={100}
+              className="cursor-pointer object-cover"
+              alt={`Charm ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
       {[...Array(5)].map((_, i) => (
-        <div key={i} className={`col-start-2 row-start-${i + 2}`}>
+        <div key={i} className={`col-start-2 row-start-${i + 3}`}>
           <div>Choose Charm {i + 1}</div>
           <CharmImages
             charmImages={charmImages}
@@ -65,7 +122,7 @@ export default function CharmBar() {
       <div className="col-start-1 row-span-3 row-start-2">
         <CharmCanvas
           charmPositions={charmPositions}
-          backgroundImg={backgroundImg}
+          necklace={selectedNecklace}
         />
       </div>
     </div>
@@ -74,10 +131,10 @@ export default function CharmBar() {
 
 function CharmCanvas({
   charmPositions,
-  backgroundImg,
+  necklace,
 }: {
   charmPositions: (string | null)[];
-  backgroundImg: string;
+  necklace: Necklace;
 }) {
   // Define positions and rotations for charms
   const charmSettings = [
@@ -91,7 +148,7 @@ function CharmCanvas({
   return (
     <div className="relative mx-auto h-[500px] w-[500px]">
       <Image
-        src={backgroundImg}
+        src={necklace.src}
         alt="Necklace background"
         className="absolute h-full w-full object-contain"
         fill
@@ -104,8 +161,8 @@ function CharmCanvas({
             alt={`Charm ${index + 1}`}
             style={{
               position: "absolute",
-              ...charmSettings[index],
-              transform: `rotate(${charmSettings[index].rotation})`,
+              ...necklace.settings[index],
+              transform: `rotate(${necklace.settings[index].rotation})`,
             }}
             className="h-20 w-20 object-cover"
             width={150}
