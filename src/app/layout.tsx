a60f53eb9e600a86934198fd79a8951927e3e14a
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-10-31 17:22:05 +0200

    working on navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Navbar from "@/components/layout/navbar";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* <CartProvider> */}
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
         {/* </CartProvider> */}
       </body>
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,41 +1,10 @@
 "use client";
 import Link from "next/link";
-// import { ShoppingCartIcon } from "@heroicons/react/20/solid";
-import { useState, useEffect } from "react";
-// import { useCart } from "@/context/CartContext";
 
 export default function Navbar() {
-  const [isVisible, setIsVisible] = useState(true);
-  const [lastYPosition, setLastYPosition] = useState<number>(0);
-
-  // const { cart } = useCart();
-
-  function handleScroll() {
-    const windowScrollY = window.scrollY;
-
-    if (windowScrollY > lastYPosition) {
-      setIsVisible(false);
-    } else {
-      setIsVisible(true);
-    }
-
-    setLastYPosition(windowScrollY);
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lastYPosition]);
-
   return (
     <nav
-      className={`bg-gray-100 text-black fixed w-full z-10 ${
-        isVisible ? "fixed" : "hidden"
-      }`}
+      className={`bg-gray-100 text-black z-10 fixed top-0 w-full transition-all duration-300`}
     >
       <ul className="flex items-center justify-center h-20 px-20">
         <Link href="/">
