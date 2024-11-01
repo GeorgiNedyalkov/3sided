0e97d3d9965aef860d7d43ae891da56308f13c62
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2024-11-01 15:35:19 +0200

    redo modal

--- src/components/cart/cart-modal.tsx
+++ src/components/cart/cart-modal.tsx
@@ -1,42 +1,30 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { ShoppingCartIcon } from "@heroicons/react/16/solid";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
 export default function CartModal() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
-  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const openModal = () => setIsOpen(true);
 
-  const openModal = () => {
-    const modal = dialogRef.current;
-    if (modal) {
-      modal.showModal();
-    }
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    const modal = dialogRef.current;
-    if (modal) {
-      modal.close();
-    }
-    setIsOpen(false);
-  };
+  const closeModal = () => setIsOpen(false);
 
   return (
     <div>
       <button onClick={openModal} className="rounded-md border-2 border-gray-600 p-2">
         <ShoppingCartIcon className="h-6" />
       </button>
-      <dialog ref={dialogRef} open={isOpen} className="relative z-50">
-        <div className="flex h-20 w-full items-center justify-between px-8 pt-4">
-          <h2 className="text-lg font-bold">My Cart</h2>
-          <button onClick={closeModal} className="h-12 rounded-md border-2 border-gray-600 p-2">
-            <XMarkIcon className="h-6" />
-          </button>
+      {isOpen && (
+        <div className="absolute right-0 top-0 h-screen w-96 bg-slate-200">
+          <div className="flex items-center justify-between p-6">
+            <h2 className="text-lg font-bold">My Cart</h2>
+            <button onClick={closeModal} className="h-12 rounded-md border-2 border-gray-600 p-2">
+              <XMarkIcon className="h-6" />
+            </button>
+          </div>
         </div>
-      </dialog>
+      )}
     </div>
   );
 }
