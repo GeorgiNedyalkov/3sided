9e36dd381d082147a2c61f282263175461f2f8d8
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2025-04-23 02:04:20 +0300

    remove search page contents

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,3 @@
-// import { getCollectionProducts } from "@/lib/shopify";
-
-// export default async function CategoryPage(props: {
-//   params: Promise<{ collection: string }>;
-//   searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
-// }) {
-//   return <section>This is the search section</section>;
-// }
+export default async function CategoryPage() {
+  return <section>This is the search section</section>;
+}
