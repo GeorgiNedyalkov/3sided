ea7a333a99afc43279b3314ad28c863f73bc1df1
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2025-04-23 02:06:54 +0300

    bugfix shop products grid getPoduct function did not have arguments

--- src/app/shop/products-grid.tsx
+++ src/app/shop/products-grid.tsx
@@ -2,14 +2,14 @@ import CharmCard from "@/components/charms/charm-card";
 import { getProducts } from "@/lib/shopify";
 
 export default async function ProductsGrid() {
-  const products = await getProducts();
+  const products = await getProducts({});
   return (
     <div className="grid grid-cols-2 gap-1 lg:grid-cols-4 lg:gap-4">
       {[...products, ...products].map((product, index) => (
         <div key={product.id + "-" + index}>
           <CharmCard
             description={product.description}
-            imageSrc={product.images.edges[0].node.url}
+            imageSrc={product.featuredImage.url}
             price={Number(product.priceRange.maxVariantPrice.amount)}
             label={product.handle}
             status="Sale"
