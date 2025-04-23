7cd2d0b0e5cdabd5c111983123659c7eaadb359a
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2025-04-23 10:30:11 +0300

    changed charms array and selecting charms ring

--- src/components/charm-bar/charm-position-selector.tsx
+++ src/components/charm-bar/charm-position-selector.tsx
@@ -50,7 +50,7 @@ export default function CharmPositionSelector({
             <button
               onClick={() => onSelectPosition(index)}
               className={clsx(
-                "absolute h-8 w-8 rounded-full ring-1 ring-stone-200 md:h-16 md:w-16",
+                "absolute h-8 w-8 rounded-full ring-1 md:h-16 md:w-16",
                 selectedCharmPosition === index ? "ring-black" : "ring-stone-200"
               )}
               style={{
--- src/components/charm-bar/charms.tsx
+++ src/components/charm-bar/charms.tsx
@@ -12,22 +12,20 @@ export default function CharmSelector({
 }) {
   return (
     <ul className="grid grid-cols-3 gap-2 md:grid-cols-5">
-      {[...charms, ...charms, ...charms, ...charms, ...charms, ...charms, ...charms, ...charms].map(
-        (charm, index) => (
-          <li
-            key={index}
-            className="relative h-16 w-16 cursor-pointer lg:h-20 lg:w-20"
-            onClick={() => onSelect(charm, position)}
-          >
-            <Image
-              src={charm.featuredImage.url}
-              alt={charm.title}
-              className="h-full w-full object-cover"
-              fill
-            />
-          </li>
-        )
-      )}
+      {[...charms].map((charm, index) => (
+        <li
+          key={index}
+          className="relative h-16 w-16 cursor-pointer lg:h-20 lg:w-20"
+          onClick={() => onSelect(charm, position)}
+        >
+          <Image
+            src={charm.featuredImage.url}
+            alt={charm.title}
+            className="h-full w-full object-cover"
+            fill
+          />
+        </li>
+      ))}
     </ul>
   );
 }
