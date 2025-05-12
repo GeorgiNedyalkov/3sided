23462004434e2947b638cca978e61d1cdd17e52e
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2025-05-12 15:36:28 +0300

    fix: remove ' in paragraph for now

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,7 @@ export default function ContactPage() {
 	return (
 		<div className="mt-20 h-screen flex flex-col items-center justify-start gap-4">
 			<h1 className="text-4xl font-semibold text-primary">Contact us</h1>
-			<p>Please don't hesitate to contact us today.</p>
-		
+			<p>Please dont hesitate to contact us today.</p>
 			<ContactForm/>
 		</div>
 	);
