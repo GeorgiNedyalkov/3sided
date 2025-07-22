06a4d22c729587f241266a9db6cebb8119164254
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2025-07-22 18:24:16 +0300

    bugfix: add to cart button

--- src/components/add-to-cart-button.tsx
+++ src/components/add-to-cart-button.tsx
@@ -6,21 +6,19 @@ import { Product } from "@/lib/shopify/types";
 import { addItem } from "./cart/actions";
 
 export function AddToCartButton({ product }: { product: Product }) {
+  const { variants } = product;
   const { addCartItem } = useCart();
   const [message, formAction] = useActionState(addItem, null);
 
-  const { variants } = product;
-
-  console.log({ variants })
-
-  const defaultVariantId = variants.length === 1 ? variants[0].id : undefined;
+  const defaultVariantId = variants.length === 1 ? variants[0]?.id : undefined;
   const actionWithId = formAction.bind(null, defaultVariantId!);
+  const finalVariant = variants.find((variant) => variant.id === defaultVariantId)!;
 
   return (
     <form
       action={async () => {
         // This updates the client state and then we need to update the server state
-        addCartItem(defaultVariantId, product);
+        addCartItem(finalVariant, product);
         await actionWithId();
       }}>
 
