888846d6e27f3e39a3d6909b95f5887432124f58
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2025-07-31 18:13:02 +0300

    add collections responsive desing

--- src/components/hero/hero-section.tsx
+++ src/components/hero/hero-section.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
     <section className="relative h-screen w-full">
 
       <video
-        className="hidden md:block absolute left-0 top-0 h-full w-full object-cover"
+        className="hidden md:block absolute bg-slate-700 w-full h-full left-0 top-0 object-cover"
         autoPlay
         loop
         muted
@@ -24,24 +24,18 @@ export default function HeroSection() {
         muted
         playsInline
       >
-        <source src="/videos/hero_mobile.mp4" type="video/mp4" />
+        <source src="/videos/hero_mobile_compressed.mp4" type="video/mp4" />
       </video>
 
-
       <div className="relative z-10 flex h-full flex-col items-center justify-center px-4 text-center text-white">
-
         <LogoMain className="-mb-32 text-xl font-bold md:text-6xl" />
-
         <p className="mb-5 mt-4 text-lg md:text-xl">
           {t("heroParagraph")}
         </p>
-
         <LinkButton href="/charm-bar">
           {t("CharmbarButton")}
         </LinkButton>
-
       </div>
-
     </section>
   );
 }
--- src/components/home/collections-section.tsx
+++ src/components/home/collections-section.tsx
@@ -57,19 +57,19 @@ const collectionsSlide2: CollectionsProps = [
 		id: "devine",
 		name: "Divine",
 		link: "/catalogue/girls",
-		imageSrc: "/collections/girls.webp"
+		imageSrc: "/collections/divine.jpg"
 	},
 	{
 		id: "flowers",
 		name: "Flowers",
 		link: "/catalogue/plants",
-		imageSrc: "/collections/diamond.jpg"
+		imageSrc: "/collections/flowers.jpg"
 	},
 	{
 		id: "animals",
 		name: "Animals",
 		link: "/catalogue/Animal",
-		imageSrc: "/collections/girls.webp"
+		imageSrc: "/collections/animals.jpg"
 	},
 ]
 
@@ -77,20 +77,38 @@ const collectionsSlide2: CollectionsProps = [
 export default function CollectionsSection() {
 	const t = useTranslations()
 	return (
-		<section className="px-4 bg-primary text-white lg:p-20 ">
-			<SectionTitle>{t("Collections")}</SectionTitle>
-			<Swiper
-				navigation={true}
-				modules={[Navigation]}
-			>
-				<SwiperSlide>
-					<Collections collections={collectionsSlide1} />
-				</SwiperSlide>
-				<SwiperSlide>
-					<Collections collections={collectionsSlide2} />
-				</SwiperSlide>
-			</Swiper>
-		</section>
+		<>
+			<section className="block md:hidden py-10 px-4 bg-primary text-white lg:p-20 ">
+				<SectionTitle>{t("Collections")}</SectionTitle>
+				<Swiper
+					navigation={true}
+					modules={[Navigation]}
+				>
+					{
+						collectionsSlide2.map((collection) => (
+
+							<SwiperSlide>
+								<Collection key={collection} collection={collection} />
+							</SwiperSlide>
+						))
+					}
+				</Swiper>
+			</section>
+			<section className="hidden md:block px-4 bg-primary text-white lg:p-20 ">
+				<SectionTitle>{t("Collections")}</SectionTitle>
+				<Swiper
+					navigation={true}
+					modules={[Navigation]}
+				>
+					<SwiperSlide>
+						<Collections collections={collectionsSlide1} />
+					</SwiperSlide>
+					<SwiperSlide>
+						<Collections collections={collectionsSlide2} />
+					</SwiperSlide>
+				</Swiper>
+			</section>
+		</>
 	)
 }
 
@@ -105,22 +123,37 @@ function Collections({ collections }: {
 	return (
 		< ul className="grid px-20 grid-cols-1 md:grid-cols-2 md:gap-20">
 			{collections.map((collection) => (
-				<Link key={collection.id} href={collection.link} className="w-full">
-					<div className="relative h-[35vh] w-[35vw] w-full bg-stone-600">
-						{/* TODO: optimize images */}
-						<Image
-							src={collection.imageSrc}
-							alt={`Charms from a  ${collection.name} collection`}
-							className="object-cover"
-							fill
-						/>
-					</div>
-					<h3 className="py-4 text-center text-2xl font-semibold">{collection.name}</h3>
-				</Link>
+				<Collection key={collection.id} collection={collection} />
 			))}
 		</ul>
 	)
 }
+
+function Collection({ collection }: {
+	collection: {
+		id: string,
+		name: string,
+		link: string,
+		imageSrc: string
+	}
+}) {
+	return (
+		<Link href={collection.link} className="w-full">
+			<div className="relative h-[35vh] w-[35vw] w-full bg-stone-600">
+				{/* TODO: optimize images */}
+				<Image
+					src={collection.imageSrc}
+					alt={`Charms from a  ${collection.name} collection`}
+					className="object-cover"
+					fill
+				/>
+			</div>
+			<h3 className="py-4 text-center text-2xl font-semibold">{collection.name}</h3>
+		</Link>
+	)
+}
+
+
 
 function SectionTitle({ children }: { children: React.ReactNode }) {
 	return <h2 className="pb-10 text-6xl font-bold">{children}</h2>
