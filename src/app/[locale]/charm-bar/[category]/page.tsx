1eea1dcf23203ab60d774930a0df83b04df9c34e
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2025-09-02 15:33:08 +0300

    remove layout

--- src/app/[locale]/charm-bar/[category]/page.tsx
+++ src/app/[locale]/charm-bar/[category]/page.tsx
@@ -3,21 +3,43 @@ import ChainSizeSelector from "@/components/charm-bar/chain-size-selector";
 import { getProduct, getProducts } from "@/lib/shopify";
 import { CharmBarContextProvider } from "@/components/charm-bar/charm-bar-context"
 
+import Breadcrumbs from "@/components/breadcrumbs";
+import { getTranslations } from "next-intl/server";
+
+
 export default async function CategorySelectPage({ params }: { params: Promise<{ category: string; }> }) {
 	const { category } = await params;
 	const chains = await getProducts({ query: `product_type:${category}` });
+	const t = await getTranslations("Charmbar");
+
+	const breadCrumbs = [
+		{
+			label: t("category"),
+			href: "/charm-bar",
+			active: false,
+		},
+		{
+			label: t("chainSizeTitle"),
+			href: "#",
+			active: true,
+		},
+	]
 
 	return (
-		<CharmBarContextProvider>
-			<div className="flex flex-col md:flex-row gap-10">
-				<div className="order-2 md:order-1">
-					<ChainSelector chains={chains} category={category} />
-				</div>
+		<>
+			<Breadcrumbs breadcrumbs={breadCrumbs} />
+			<p className="p-2 text-sm">{t("chainSizeStep")}</p>
+			<CharmBarContextProvider>
+				<div className="flex flex-col md:flex-row gap-10">
+					<div className="order-2 md:order-1">
+						<ChainSelector chains={chains} category={category} />
+					</div>
 
-				<div className="order-1 md:order-2">
-					<ChainSizeSelector category={category} />
+					<div className="order-1 md:order-2">
+						<ChainSizeSelector category={category} />
+					</div>
 				</div>
-			</div>
-		</CharmBarContextProvider>
+			</CharmBarContextProvider>
+		</>
 	);
 }
